8bc325aad62259ac04aa5d86d4b16753936fda6c
Tomás Migone <tomasmigone@gmail.com>
2020-06-12 10:26:31 -0300

    types: added boolean
    Signed-off-by: Tomás Migone <tomasmigone@gmail.com>

---
+++ src/tags/boolean.ts
@@ -0,0 +1,25 @@
+import { PATag, PATagType } from './tag'
+
+// PulseAudio boolean tag structure by section
+// - 1 byte: tag type
+export default class PABoolean extends PATag<boolean> {
+  type: PATagType = PATagType.PA_TAG_BOOLEAN
+
+  toTagBuffer(value: boolean): Buffer {
+    const boolValue: PATagType = value ? PATagType.PA_TAG_BOOLEAN_TRUE : PATagType.PA_TAG_BOOLEAN_FALSE
+    const buffer: Buffer = Buffer.allocUnsafe(1)
+    let offset: number = 0
+    offset = buffer.writeUInt8(boolValue.toString().charCodeAt(0), offset)
+    return buffer
+  }
+
+  fromTagBuffer(buffer: Buffer): boolean {
+    // TODO: Validate buffer
+    return buffer.readUInt8(0) === PATagType.PA_TAG_BOOLEAN_TRUE.toString().charCodeAt(0)
+  }
+
+  /* @ts-ignore */
+  isTagBuffer(buffer: Buffer): boolean {
+    return true 
+  }
+}
--- test/tags.spec.ts
+++ test/tags.spec.ts
@@ -1,6 +1,5 @@
 import test from 'ava'
-import { PA_TAG_TYPES } from '../src/tags/tag'
-import { PATag, PA_U32, PA_Arbitrary } from "../src/tags";
+import { PATag, PATagType, PAU32, PAArbitrary, PABoolean } from "../src/tags"
 
 interface PATagTestCases<T> {
   title: string
@@ -9,7 +8,7 @@ interface PATagTestCases<T> {
   expected: {
     tag: Buffer
     size: number
-    type: PA_TAG_TYPES
+    type: PATagType
     value: T
   }
 }
@@ -18,47 +17,69 @@ const cases: PATagTestCases<any>[] = [
   {
     title: 'Create u32 from value',
     init: 120,
-    pa_tag: new PA_U32(120),
+    pa_tag: new PAU32(120),
     expected: {
       tag: Buffer.from('4c00000078', 'hex'),
       size: 5,
-      type: PA_TAG_TYPES.PA_TAG_U32,
+      type: PATagType.PA_TAG_U32,
       value: 120
     }
   },
   {
     title: 'Create u32 from buffer',
     init: Buffer.from('4c00000078', 'hex'),
-    pa_tag: new PA_U32(Buffer.from('4c00000078', 'hex')),
+    pa_tag: new PAU32(Buffer.from('4c00000078', 'hex')),
     expected: {
       tag: Buffer.from('4c00000078', 'hex'),
       size: 5,
-      type: PA_TAG_TYPES.PA_TAG_U32,
+      type: PATagType.PA_TAG_U32,
       value: 120
     }
   },
   {
     title: 'Create arbitrary from value',
     init: Buffer.from('59cf1c70a28fa55cc512a36d204963e8ce8b1ccfec5ef095d9b86f1f46f88aea9cb7ea18aa7d163174353379a277ea83867f5e36560749b235f025b48da7dadc7033921b820af1c4434f3dfc196a89f8bdd7357e40dcd741ddeccbc9c5c70cf966e863fef08746912ef2550b0068f4f343f5f97a0bcedae38d66b6483f13dd6af6051ad7b44cae1322e2f1cb2ea4d4ce280cd1775327b36fadf5c5a191758a1dcb02627ddb87e376ef02603c61639b323547359dace2d7a1b1471a0599baa37a2184c0f1a849a4fbccb943a80fb7dbe4619c9e1437bc0fac2811aed25a6305ebc2a23afbc04a4e206445cb20ce8cb2d6045fc158f11533ab0c20a0cfc6c93074', 'hex'),
-    pa_tag: new PA_Arbitrary(Buffer.from('59cf1c70a28fa55cc512a36d204963e8ce8b1ccfec5ef095d9b86f1f46f88aea9cb7ea18aa7d163174353379a277ea83867f5e36560749b235f025b48da7dadc7033921b820af1c4434f3dfc196a89f8bdd7357e40dcd741ddeccbc9c5c70cf966e863fef08746912ef2550b0068f4f343f5f97a0bcedae38d66b6483f13dd6af6051ad7b44cae1322e2f1cb2ea4d4ce280cd1775327b36fadf5c5a191758a1dcb02627ddb87e376ef02603c61639b323547359dace2d7a1b1471a0599baa37a2184c0f1a849a4fbccb943a80fb7dbe4619c9e1437bc0fac2811aed25a6305ebc2a23afbc04a4e206445cb20ce8cb2d6045fc158f11533ab0c20a0cfc6c93074', 'hex')),
+    pa_tag: new PAArbitrary(Buffer.from('59cf1c70a28fa55cc512a36d204963e8ce8b1ccfec5ef095d9b86f1f46f88aea9cb7ea18aa7d163174353379a277ea83867f5e36560749b235f025b48da7dadc7033921b820af1c4434f3dfc196a89f8bdd7357e40dcd741ddeccbc9c5c70cf966e863fef08746912ef2550b0068f4f343f5f97a0bcedae38d66b6483f13dd6af6051ad7b44cae1322e2f1cb2ea4d4ce280cd1775327b36fadf5c5a191758a1dcb02627ddb87e376ef02603c61639b323547359dace2d7a1b1471a0599baa37a2184c0f1a849a4fbccb943a80fb7dbe4619c9e1437bc0fac2811aed25a6305ebc2a23afbc04a4e206445cb20ce8cb2d6045fc158f11533ab0c20a0cfc6c93074', 'hex')),
     expected: {
       tag: Buffer.from('780000010059cf1c70a28fa55cc512a36d204963e8ce8b1ccfec5ef095d9b86f1f46f88aea9cb7ea18aa7d163174353379a277ea83867f5e36560749b235f025b48da7dadc7033921b820af1c4434f3dfc196a89f8bdd7357e40dcd741ddeccbc9c5c70cf966e863fef08746912ef2550b0068f4f343f5f97a0bcedae38d66b6483f13dd6af6051ad7b44cae1322e2f1cb2ea4d4ce280cd1775327b36fadf5c5a191758a1dcb02627ddb87e376ef02603c61639b323547359dace2d7a1b1471a0599baa37a2184c0f1a849a4fbccb943a80fb7dbe4619c9e1437bc0fac2811aed25a6305ebc2a23afbc04a4e206445cb20ce8cb2d6045fc158f11533ab0c20a0cfc6c93074', 'hex'),
       size: 261,
-      type: PA_TAG_TYPES.PA_TAG_ARBITRARY,
+      type: PATagType.PA_TAG_ARBITRARY,
       value: Buffer.from('59cf1c70a28fa55cc512a36d204963e8ce8b1ccfec5ef095d9b86f1f46f88aea9cb7ea18aa7d163174353379a277ea83867f5e36560749b235f025b48da7dadc7033921b820af1c4434f3dfc196a89f8bdd7357e40dcd741ddeccbc9c5c70cf966e863fef08746912ef2550b0068f4f343f5f97a0bcedae38d66b6483f13dd6af6051ad7b44cae1322e2f1cb2ea4d4ce280cd1775327b36fadf5c5a191758a1dcb02627ddb87e376ef02603c61639b323547359dace2d7a1b1471a0599baa37a2184c0f1a849a4fbccb943a80fb7dbe4619c9e1437bc0fac2811aed25a6305ebc2a23afbc04a4e206445cb20ce8cb2d6045fc158f11533ab0c20a0cfc6c93074', 'hex')
     }
   },
   {
     title: 'Create arbitrary from buffer',
     init: Buffer.from('780000010059cf1c70a28fa55cc512a36d204963e8ce8b1ccfec5ef095d9b86f1f46f88aea9cb7ea18aa7d163174353379a277ea83867f5e36560749b235f025b48da7dadc7033921b820af1c4434f3dfc196a89f8bdd7357e40dcd741ddeccbc9c5c70cf966e863fef08746912ef2550b0068f4f343f5f97a0bcedae38d66b6483f13dd6af6051ad7b44cae1322e2f1cb2ea4d4ce280cd1775327b36fadf5c5a191758a1dcb02627ddb87e376ef02603c61639b323547359dace2d7a1b1471a0599baa37a2184c0f1a849a4fbccb943a80fb7dbe4619c9e1437bc0fac2811aed25a6305ebc2a23afbc04a4e206445cb20ce8cb2d6045fc158f11533ab0c20a0cfc6c93074', 'hex'),
-    pa_tag: new PA_Arbitrary(Buffer.from('780000010059cf1c70a28fa55cc512a36d204963e8ce8b1ccfec5ef095d9b86f1f46f88aea9cb7ea18aa7d163174353379a277ea83867f5e36560749b235f025b48da7dadc7033921b820af1c4434f3dfc196a89f8bdd7357e40dcd741ddeccbc9c5c70cf966e863fef08746912ef2550b0068f4f343f5f97a0bcedae38d66b6483f13dd6af6051ad7b44cae1322e2f1cb2ea4d4ce280cd1775327b36fadf5c5a191758a1dcb02627ddb87e376ef02603c61639b323547359dace2d7a1b1471a0599baa37a2184c0f1a849a4fbccb943a80fb7dbe4619c9e1437bc0fac2811aed25a6305ebc2a23afbc04a4e206445cb20ce8cb2d6045fc158f11533ab0c20a0cfc6c93074', 'hex')),
+    pa_tag: new PAArbitrary(Buffer.from('780000010059cf1c70a28fa55cc512a36d204963e8ce8b1ccfec5ef095d9b86f1f46f88aea9cb7ea18aa7d163174353379a277ea83867f5e36560749b235f025b48da7dadc7033921b820af1c4434f3dfc196a89f8bdd7357e40dcd741ddeccbc9c5c70cf966e863fef08746912ef2550b0068f4f343f5f97a0bcedae38d66b6483f13dd6af6051ad7b44cae1322e2f1cb2ea4d4ce280cd1775327b36fadf5c5a191758a1dcb02627ddb87e376ef02603c61639b323547359dace2d7a1b1471a0599baa37a2184c0f1a849a4fbccb943a80fb7dbe4619c9e1437bc0fac2811aed25a6305ebc2a23afbc04a4e206445cb20ce8cb2d6045fc158f11533ab0c20a0cfc6c93074', 'hex')),
     expected: {
       tag: Buffer.from('780000010059cf1c70a28fa55cc512a36d204963e8ce8b1ccfec5ef095d9b86f1f46f88aea9cb7ea18aa7d163174353379a277ea83867f5e36560749b235f025b48da7dadc7033921b820af1c4434f3dfc196a89f8bdd7357e40dcd741ddeccbc9c5c70cf966e863fef08746912ef2550b0068f4f343f5f97a0bcedae38d66b6483f13dd6af6051ad7b44cae1322e2f1cb2ea4d4ce280cd1775327b36fadf5c5a191758a1dcb02627ddb87e376ef02603c61639b323547359dace2d7a1b1471a0599baa37a2184c0f1a849a4fbccb943a80fb7dbe4619c9e1437bc0fac2811aed25a6305ebc2a23afbc04a4e206445cb20ce8cb2d6045fc158f11533ab0c20a0cfc6c93074', 'hex'),
       size: 261,
-      type: PA_TAG_TYPES.PA_TAG_ARBITRARY,
+      type: PATagType.PA_TAG_ARBITRARY,
       value: Buffer.from('59cf1c70a28fa55cc512a36d204963e8ce8b1ccfec5ef095d9b86f1f46f88aea9cb7ea18aa7d163174353379a277ea83867f5e36560749b235f025b48da7dadc7033921b820af1c4434f3dfc196a89f8bdd7357e40dcd741ddeccbc9c5c70cf966e863fef08746912ef2550b0068f4f343f5f97a0bcedae38d66b6483f13dd6af6051ad7b44cae1322e2f1cb2ea4d4ce280cd1775327b36fadf5c5a191758a1dcb02627ddb87e376ef02603c61639b323547359dace2d7a1b1471a0599baa37a2184c0f1a849a4fbccb943a80fb7dbe4619c9e1437bc0fac2811aed25a6305ebc2a23afbc04a4e206445cb20ce8cb2d6045fc158f11533ab0c20a0cfc6c93074', 'hex')
     }
-   }
+   },
+   {
+    title: 'Create boolean from value',
+    init: false,
+    pa_tag: new PABoolean(false),
+    expected: {
+      tag: Buffer.from('30', 'hex'),
+      size: 1,
+      type: PATagType.PA_TAG_BOOLEAN,
+      value: false
+    }
+  },
+  {
+    title: 'Create boolean from buffer',
+    init: Buffer.from('30', 'hex'),
+    pa_tag: new PABoolean(Buffer.from('30', 'hex')),
+    expected: {
+      tag: Buffer.from('30', 'hex'),
+      size: 1,
+      type: PATagType.PA_TAG_BOOLEAN,
+      value: false
+    }
+  }
 ]
 
 for (const c of cases) {
